b33cdd9f92bbc625fd41bf4334abeed35c68e574
Linkbro1 <104574466+Linkbro1@users.noreply.github.com>
2025-09-21 23:12:42 +0100

    first actual commit, setting groundwork, and adapting the neoforge template to my own uses.

---
+++ src/main/java/net/linkbro/createsignalsandstates/Config.java
@@ -0,0 +1,11 @@
+package net.linkbro.createsignalsandstates;
+
+import net.neoforged.neoforge.common.ModConfigSpec;
+
+public class Config {
+    private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
+
+    // will be filled with config as it becomes necessary.
+
+    static final ModConfigSpec SPEC = BUILDER.build();
+}
---
+++ src/main/java/net/linkbro/createsignalsandstates/CreateSignalsAndStates.java
@@ -0,0 +1,33 @@
+package net.linkbro.createsignalsandstates;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+
+@Mod(CreateSignalsAndStates.MODID)
+public class CreateSignalsAndStates {
+    public static final String MODID = "createsignalsandstates";
+
+    public CreateSignalsAndStates(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+
+    }
+
+    @EventBusSubscriber(modid = CreateSignalsAndStates.MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    static class ClientModEvents {
+        @SubscribeEvent
+        static void onClientSetup(FMLClientSetupEvent event) {
+        }
+    }
+}
